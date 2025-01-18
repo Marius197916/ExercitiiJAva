dd409b58a206466334b8abc748703807731ea436
Marius <mariusmihalache08@gmail.com>
2025-01-18 19:02:19 +0200

    ExercitiiJava

---
+++ src/SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 24;
+        int b = 26;
+        int suma = a + b;
+
+        System.out.println("Suma lui a și b: " + suma);
+    }
+}
