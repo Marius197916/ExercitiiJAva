a015389b8a35f37b2c4d442db8672ba6780d6b6b
Marius <mariusmihalache08@gmail.com>
2025-01-18 19:12:52 +0200

    ExercitiiJava

---
+++ src/DivideTwoNumbers.java
@@ -0,0 +1,9 @@
+public class DivideTwoNumbers {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 3;
+        int div = a / b;
+
+        System.out.println("Rezultatul împărțirii lui a și b: " + div);
+    }
+}
